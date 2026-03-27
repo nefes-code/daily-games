cb71bcf00377449f2d4175cd7d144c552300d6b4
pedrollcopatti <pedrolucascopatti@gmail.com>
2026-03-27 18:06:40 -0300

    again fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   reactCompiler: true,
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
+  },
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./src/generated/prisma/*.node"],
+    "/games/**/*": ["./src/generated/prisma/*.node"],
   },
   images: {
     remotePatterns: [
