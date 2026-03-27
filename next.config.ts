f7bec41c62acf7c5837e13234979aea122a28888
pedrollcopatti <pedrolucascopatti@gmail.com>
2026-03-27 17:54:31 -0300

    fix again

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   reactCompiler: true,
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
